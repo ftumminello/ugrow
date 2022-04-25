c078823e44beb7bfaa677d7433ada149b2358f4c
ftumminello <frankietumminello@gmail.com>
2022-04-25 13:50:51 -0400

    sleep function

--- readExample.py
+++ readExample.py
@@ -4,4 +4,4 @@ while 1:
     with open("out.log", "r") as f:
         content = f.read()
         print(content)
-    sleep(5) # suspend execution for 5 seconds
+    time.sleep(5) # suspend execution for 5 seconds
